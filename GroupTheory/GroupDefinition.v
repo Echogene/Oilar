07eba56dfaf67591b3d93f3ac03ef0bce6e56e02
Echogene <echogene.alpha@gmail.com>
2021-01-01 01:00:51 +0000

    The product of two abelian groups is abelian

--- GroupTheory/GroupDefinition.v
+++ GroupTheory/GroupDefinition.v
@@ -4,36 +4,35 @@ Generalizable Variables G.
 
 (* http://www.eelis.net/research/math-classes/mscs.pdf *)
 
-Class SemiGroupOp A := g_op: A → A → A.
-Class GroupInv A := g_i: A → A.
-Class GroupId A := g_e: A.
+Class SemiGroupOp A := sg_op: A → A → A.
 
-Class SemiGroup (G : Set) {op : SemiGroupOp G}: Prop := {
+(* Add an operator for readability. *)
+Notation "a · b" := (sg_op a b) (at level 50, left associativity).
+
+Class SemiGroup {G : Set} {op : SemiGroupOp G}: Prop := {
   (* The group operator is associative. *)
   sg_assoc : ∀ a b c, op (op a b) c = op a (op b c);
 }.
 
-Class Group (G : Set) {op : SemiGroupOp G} {i : GroupInv G} {e: GroupId G}: Prop := {
-  g_sg :> SemiGroup G;
+Class GroupInv A := g_i: A → A.
+
+(* Add a more familiar notation for inverse. *)
+Notation "a ⁻¹" := (g_i a) (at level 40).
+
+Class GroupId A := g_e: A.
+
+Class Group {G : Set} {op : SemiGroupOp G} {i : GroupInv G} {e: GroupId G}: Prop := {
+  g_sg :> SemiGroup;
   (* e is the right-identity for all elements. *)
   g_right_identity : ∀ a, op a e = a;
   (* a⁻¹ is the right-inverse of a for all elements. *)
   g_right_inverse : ∀ a, op a (i a) = e;
 }.
 
-Instance groups_are_semigroups `{Group G}: SemiGroup G.
-Proof.
-  exact g_sg.
-Qed.
-
-(* Add an operator for readability. *)
-Notation "a · b" := (g_op a b) (at level 50, left associativity).
-
-(* Add a more familiar notation for inverse. *)
-Notation "a ⁻¹" := (g_i a) (at level 40).
-
-Class AbelianGroup (G : Set) {op : SemiGroupOp G} {i : GroupInv G} {e: GroupId G}: Prop := {
-  ag_sg :> Group G;
+Class AbelianGroup {G : Set} {op : SemiGroupOp G} {i : GroupInv G} {e: GroupId G}: Prop := {
+  ag_g :> Group;
   (* The operation is commutative. *)
   ag_commutative : ∀ a b, op a b = op b a;
 }.
+
+Instance AsGroup `(A: AbelianGroup): Group := (@ag_g _ _ _ _ A).
--- GroupTheory/GroupInstances.v
+++ GroupTheory/GroupInstances.v
@@ -2,7 +2,7 @@ Require Import Utf8.
 Require Import GroupDefinition.
 Require Import Setoid.
 
-(* The group with one element is a group. *)
+(* The group with one element is an abelian group. *)
 
 Inductive One : Set :=
   | e1 : One.
@@ -10,7 +10,7 @@ Inductive One : Set :=
 Definition C₁_op : SemiGroupOp One := fun (a: One) (b: One) => e1.
 Definition C₁_i : GroupInv One := fun (a: One) => e1.
 
-Instance C₁ : @Group One C₁_op C₁_i e1.
+Instance C₁ : @AbelianGroup One C₁_op C₁_i e1.
 Proof.
   repeat split.
   intros a.
@@ -18,7 +18,7 @@ Proof.
   simpl. reflexivity.
 Qed.
 
-(* The cyclic group with two elements is a group. *)
+(* The cyclic group with two elements is an abelian group. *)
 
 Inductive Two : Set :=
   | zero : Two
@@ -37,27 +37,35 @@ Definition C₂_i : GroupInv Two := fun (a: Two) =>
   | one => one
   end.
 
-Instance C₂ : @Group Two C₂_op C₂_i zero.
+Instance C₂ : @AbelianGroup Two C₂_op C₂_i zero.
 Proof.
   repeat split.
   intros a b c.
   destruct a.
-  simpl. reflexivity.
+  all: simpl.
+  reflexivity.
   destruct b.
   destruct c.
-  simpl. reflexivity.
-  simpl. reflexivity.
+  all : simpl.
+  reflexivity. reflexivity.
   destruct c.
-  simpl. reflexivity.
-  simpl. reflexivity.
+  reflexivity. reflexivity.
   intros a.
   destruct a.
-  simpl. reflexivity.
-  simpl. reflexivity.
+  all: simpl.
+  reflexivity. reflexivity.
   intros a.
   destruct a.
-  simpl. reflexivity.
-  simpl. reflexivity.
+  all: simpl.
+  reflexivity. reflexivity.
+  intros a b.
+  destruct a.
+  destruct b.
+  all: simpl.
+  reflexivity. reflexivity.
+  destruct b.
+  all: simpl.
+  reflexivity. reflexivity.
 Qed.
 
 (* The direct product of two groups is a group. *)
@@ -79,8 +87,8 @@ Definition GroupProduct_i
   end.
 
 Instance GroupProduct
-    (G : Set) {g_op : SemiGroupOp G} {g_i : GroupInv G} {g_e: GroupId G} `{P : @Group G g_op g_i g_e}
-    (H : Set) {h_op : SemiGroupOp H} {h_i : GroupInv H} {h_e: GroupId H} `{Q : @Group H h_op h_i h_e}
+    {G : Set} {g_op : SemiGroupOp G} {g_i : GroupInv G} {g_e: GroupId G} (P : @Group G g_op g_i g_e)
+    {H : Set} {h_op : SemiGroupOp H} {h_i : GroupInv H} {h_e: GroupId H} (Q : @Group H h_op h_i h_e)
     : (@Group
         (prod G H)
         (@GroupProduct_op G g_op g_i g_e P H h_op h_i h_e Q)
@@ -108,10 +116,48 @@ Proof.
   reflexivity.
 Qed.
 
-Notation "A × B" := (GroupProduct A B) (at level 30, right associativity).
+Instance AbelianGroupProduct
+    {G : Set} {g_op : SemiGroupOp G} {g_i : GroupInv G} {g_e: GroupId G} (P : @AbelianGroup G g_op g_i g_e)
+    {H : Set} {h_op : SemiGroupOp H} {h_i : GroupInv H} {h_e: GroupId H} (Q : @AbelianGroup H h_op h_i h_e)
+    : (@AbelianGroup
+        (prod G H)
+        (@GroupProduct_op G g_op g_i g_e (AsGroup P) H h_op h_i h_e (AsGroup Q))
+        (@GroupProduct_i G g_op g_i g_e (AsGroup P) H h_op h_i h_e (AsGroup Q))
+        (g_e, h_e)
+    ).
+Proof.
+  repeat split.
+  intros a b c.
+  compute.
+  destruct a.
+  destruct b.
+  destruct c.
+  repeat rewrite sg_assoc.
+  reflexivity.
+  intros a.
+  compute.
+  destruct a.
+  repeat rewrite g_right_identity.
+  reflexivity.
+  intros a.
+  compute.
+  destruct a.
+  repeat rewrite g_right_inverse.
+  reflexivity.
+  intros a b.
+  compute.
+  destruct a.
+  destruct b.
+  rewrite ag_commutative.
+  replace (h_op h h0) with (h_op h0 h).
+  reflexivity.
+  rewrite ag_commutative.
+  reflexivity.
+Qed.
 
+Notation "A × B" := (AbelianGroupProduct A B) (at level 30, right associativity).
 
+(* The Klein four-group is the product of C₂ and C₂. *)
 
-
-
+Definition K₄ := C₂ × C₂.
 
